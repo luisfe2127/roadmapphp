4fd31e5c0d529184e7ecc37d102fabfe8d1ac243
Luís Felipe <luis.fe8000@gmail.com>
2025-08-05 23:03:57 -0300

    atualizando escopo02

--- exercicios/exercicio01/escopo02.php
+++ exercicios/exercicio01/escopo02.php
@@ -1,3 +1,25 @@
 <?php 
-//Escopo Funçao
+function test01() {
+    $a = 0;
+    echo $a . "</br>";
+    $a++;
+}
+
+test01();
+test01();
+test01();
+
+echo "</br>";
+
+function test02() {
+    static $a = 0;
+    echo $a . "</br>";
+    $a++;
+}
+
+test02();
+test02();
+test02();
+
+
 ?>
