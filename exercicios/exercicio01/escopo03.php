060f1548ec455c668475cce90b31a1cbddc1b94b
Luís Felipe <luis.fe8000@gmail.com>
2025-09-24 23:29:36 -0300

    atualizando escopo03.php

--- exercicios/exercicio01/escopo03.php
+++ exercicios/exercicio01/escopo03.php
@@ -76,5 +76,65 @@
     };
 
     ad();
+
+    function &get_instance_ref() {
+    static $obj;
+
+    echo 'Static object: ';
+    var_dump($obj);
+    if (!isset($obj)) {
+        $new = new stdClass;
+        // Assign a reference to the static variable
+        $obj = &$new;
+    }
+    if (!isset($obj->property)) {
+        $obj->property = 1;
+    } else {
+        $obj->property++;
+    }
+    return $obj;
+}
+
+var_dump(get_instance_ref());
      
+echo "<br>";
+function &get_instance_noref() {
+    static $obj;
+
+    echo 'Static object: ';
+    var_dump($obj);
+    if (!isset($obj)) {
+        $new = new stdClass;
+        // Assign the object to the static variable
+        $obj = $new;
+    }
+    if (!isset($obj->property)) {
+        $obj->property = 1;
+    } else {
+        $obj->property++;
+    }
+    return $obj;
+}
+
+$obj1 = get_instance_ref();
+$still_obj1 = get_instance_ref();
+echo "\n";
+$obj2 = get_instance_noref();
+$still_obj2 = get_instance_noref();
+
+var_dump($obj1);
+echo "</br>";
+var_dump($obj1);
+echo "</br>";
+var_dump($still_obj1);
+echo "</br>";
+var_dump($still_obj1);
+echo "</br></br></br>";
+var_dump($obj2);
+echo "</br>";
+var_dump($obj2);
+echo "</br>";
+var_dump($still_obj2);
+echo "</br>";
+var_dump($still_obj2);
 ?>
