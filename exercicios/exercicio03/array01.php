da29809e4b0588398e6d53d71d617a90014fa539
Luís Felipe <luis.fe8000@gmail.com>
2025-09-29 23:26:08 -0300

    criando e atualizando array01

---
+++ exercicios/exercicio03/array01.php
@@ -0,0 +1,23 @@
+<?php 
+$array01 = array(1 => "a", 2 => "b", 3 => "c");
+$array02 = [1 => "a", 2 => "b", 3 => "c"];
+
+echo "<pre>";
+print_r($array01);
+print_r($array02);  
+
+$arrayString01 = ["test" => 1, 3 => "b", 4 => "c", 1 => "a"];
+
+var_dump($arrayString01);
+
+$array03 = [1,2,3,3 => "a",7,8,4 => "b", "teste" => 9, 10];
+
+print_r($array03);
+
+$arrayCar = [
+    $car = ["Toyota", "Corolla", "Lamborghini", "Tesla", $modelCar = [2020, 2021, 2022]],
+    $scotter = ["Honda", "Biz", "Yamaha", "Dafra", $modelSco = [2020, 2021, 2022]]
+];
+
+echo($car[2], $modelCar[1]);
+?>
