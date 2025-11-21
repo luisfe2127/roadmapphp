9ec141984c5a07e28cd9abbbf6f5a6e30bfbf374
Luís Felipe <luis.fe8000@gmail.com>
2025-11-20 23:12:53 -0300

    atualizando condicoes02

---
+++ exercicios/exercicio04/condicoes02.php
@@ -0,0 +1,18 @@
+<?php 
+$comida = "Fogaça";
+
+function teste() {
+    return "Comida não encontrada";
+};
+
+$valor = match($comida) {
+    "Lasanha" => "R$ 30,00",
+    "Pizza" , "Fogaça" => "R$ 25,00",
+    "Hambúrguer" => "R$ 20,00",
+    default => teste(),
+};
+
+echo "O valor da $comida é $valor </br>";
+
+
+?>
