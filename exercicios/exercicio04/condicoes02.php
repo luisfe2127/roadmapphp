5bd66e1bfd039cb3cb703879221edba84e392f2a
Luís Felipe <luis.fe8000@gmail.com>
2025-11-21 23:18:29 -0300

    atualizando condicoes03

--- exercicios/exercicio04/condicoes02.php
+++ exercicios/exercicio04/condicoes02.php
@@ -9,7 +9,7 @@ $valor = match($comida) {
     "Lasanha" => "R$ 30,00",
     "Pizza" , "Fogaça" => "R$ 25,00",
     "Hambúrguer" => "R$ 20,00",
-    default => teste(),
+    default => teste()
 };
 
 echo "O valor da $comida é $valor </br>";
---
+++ exercicios/exercicio04/condicoes03.php
@@ -0,0 +1,9 @@
+<?php 
+    $name = "João";
+    $age = 1;
+
+    echo "Nome: " . ($name ?? "Nome não informado") . "</br>";
+    echo "Idade: " . ($age ?? "Idade não informada") . "</br>";
+
+
+?>
