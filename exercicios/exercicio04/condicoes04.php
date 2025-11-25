8fc6842c80156770f8f1f74d457cfecc0aa448eb
Luís Felipe <luis.fe8000@gmail.com>
2025-11-24 23:17:29 -0300

    atualizando condicoes04

---
+++ exercicios/exercicio04/condicoes04.php
@@ -0,0 +1,34 @@
+<?php 
+class People {
+    public function __construct(
+        private string $name,
+        private ?Child $child = null
+    ){      
+    }
+    
+        public function getName(): String {
+            return $this->name;
+        }
+
+        public function getChild(): ?Child {
+            return $this->child;
+        }
+}
+class Child extends People{}
+
+$people = new People("João", new Child("Maria"));
+
+if (!empty($people)) {
+    if(!empty($people->getChild())) {
+        if(!empty($people->getChild()->getName())) {
+            echo "Nome do filho: " . $people->getChild()->getName() . "</br>";
+        } else {
+            echo "Nome do filho: Nome não informado </br>";
+        }
+    }
+}
+
+$childName = $people->getChild()?->getName() ?? "Nome não informado";
+echo "Nome do filho: " . $childName . "</br>";
+
+?>
